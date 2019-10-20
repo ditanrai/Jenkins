a6f1fb0351999f152282638a813d4ab3a14f99c6
Ditan Rai <ditanrai8@gmail.com>
2019-10-19 19:46:45 -0500

    First commit

---
+++ src/AccountTest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AccountTest {
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		
+		Account myAccount = new Account();
+		
+		System.out.println("Initial name is:"+ myAccount.getName());
+		System.out.println("Please enter the name:");
+		String theName = input.nextLine();
+		
+		myAccount.setName(theName);
+		
+		System.out.println("The name in object myAccount is:" +myAccount.getName());
+				
+	}
+
+}
---
+++ src/PrintingStar.java
@@ -0,0 +1,18 @@
+
+public class PrintingStar {
+	
+	public static void printStar(int n) {
+		int i, j;
+		
+		for(i=0; i<n; i++) {
+			for(j=0; j<=i; j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String args[]) {
+		int n=8;
+		printStar(n);
+	}
+}
